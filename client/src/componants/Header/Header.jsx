2c54338c277f44d17579cd4d236caade9e682b45
MahidunNobi <119042826+MahidunNobi@users.noreply.github.com>
2023-11-30 11:48:18 +0600

    Update Header.jsx

--- client/src/componants/Header/Header.jsx
+++ client/src/componants/Header/Header.jsx
@@ -46,81 +46,18 @@ const Header = ({page}) => {
           <p className="text-2xl">
             Search deals on hotels, homes, and much more...
           </p>
+          <div className="demo text-gray-900 font-semibold">
+            <h2 className="text-2xl"> Hotels Available In:</h2>
+            <ul>              
+              <li> London </li>
+              <li> Madrid </li>
+              <li> Tokyo </li>
+            </ul>
+          </div>
         </div>}
         {page !=="hotelsList" && <HomeSearchBar page={page} />}
         
-        {/* <div className="SearchBar flex justify-around items-center bg-white w-full text-gray-400 h-[60px] border-2 border-orange-400 absolute bottom-[-25px] rounded-md">
-
-          <div className="searchInput flex items-center space-x-3 text-gray-800">
-            <FaBed  className="text-2xl"/>
-            <input type="text" placeholder="Where are you going?" name="searchInput" className="outline-none" />
-          </div>
-          <div className=" flex items-center space-x-3 cursor-pointer text-gray-800" onClick={()=> setOpenDate(!openDate)}>
-            <SlCalender  className="text-2xl"/>
-            <p> 
-              {`${format(DateRangeState[0].startDate, "MM/dd/yyyy")} 
-              to 
-              ${format(DateRangeState[0].endDate, "MM/dd/yyyy")}`}
-            </p>
-            { openDate && <DateRange
-              editableDateInputs={true}
-              onChange={item => setDateRangeState([item.selection])}
-              moveRangeOnFirstSelection={false}
-              ranges={DateRangeState}
-              className="absolute top-[60px]"
-            />}
-          </div>
-          <div className=" flex items-center space-x-3 cursor-pointer text-gray-800" >
-            <BsPerson className="text-2xl" onClick={()=>setOpenOptions(!openOptions)}/>
-            <span onClick={()=>setOpenOptions(!openOptions)}> {`${options.adult} Adult, ${options.child} Children . ${options.room} Room`}</span>
-
-            {openOptions && <div className="options w-[300px] p-6 absolute top-[60px] border border-gray-200">
-
-              <div className="optionsItem flex justify-between text-gray-600">
-                <p>Adult</p>
-                <div className="numberbtns border border-orange-200">
-                  <button className="text-lg px-2 active:text-orange-500" 
-                  disabled={options.adult <=1}                  
-                  onClick={()=> handleOptions("adult", "d")}> - </button>
-                  <span className="px-4"> {options.adult} </span>
-                  <button className="text-lg px-2 active:text-orange-500"
-                  onClick={()=> handleOptions("adult", "i")}> + </button>
-                </div>
-              </div>
-
-              <div className="optionsItem flex justify-between text-gray-600 my-6">
-                <p>Child</p>
-                <div className="numberbtns border border-orange-200">
-                  <button className="text-lg px-2 active:text-orange-500"
-                  disabled={options.child <=1} 
-                  onClick={()=> handleOptions("child", "d")}> - </button>
-                  <span className="px-4"> {options.child} </span>
-                  <button className="text-lg px-2 active:text-orange-500"
-                  onClick={()=> handleOptions("child", "i")}> + </button>
-                </div>
-              </div>
-
-              <div className="optionsItem flex justify-between text-gray-600 mb-6">
-                <p>Room</p>
-                <div className="numberbtns border border-orange-200">
-                  <button className="text-lg px-2 active:text-orange-500"
-                  disabled={options.room <=1} 
-                  onClick={()=> handleOptions("room", "d")}> - </button>
-                  <span className="px-4"> {options.room} </span>
-                  <button className="text-lg px-2 active:text-orange-500"
-                  onClick={()=> handleOptions("room", "i")}> + </button>
-                </div>
-              </div>
-              <button className="w-full text-orange-500 border-2 py-2 rounded-full border-orange-500 hover:text-white hover:bg-orange-500 duration-300" 
-              onClick={()=>setOpenOptions(!openOptions)}> Done</button>
-
-            </div>}
-          </div>
-          <div className="Btn">
-            <button className="text-orange-500 border-orange-500  border-2 hover:text-white hover:bg-orange-500 duration-300 px-6 rounded-lg py-1"> Search</button>
-          </div>
-
-        </div> */}
+       
 
       </div>      
     </div>
